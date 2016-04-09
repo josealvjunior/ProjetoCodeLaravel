fff61a7dce07952bd046ca4c88f9a3335457f970
Juninho <morbios@gmail.com>
2016-04-09 19:48:06 -0300

    Alteraçoes cripteria

--- app/Repositories/ClientRepositoryEloquent.php
+++ app/Repositories/ClientRepositoryEloquent.php
@@ -16,6 +16,11 @@ use project\Presenters\ClientPresenter;
 
 class ClientRepositoryEloquent extends BaseRepository implements ClientRepository
 {
+    protected $fieldSearchable= [
+      'name',
+        'email'
+    ];
+
     public function model()
     {
      return Client::class;
@@ -24,5 +29,10 @@ class ClientRepositoryEloquent extends BaseRepository implements ClientRepositor
     public function presenter()
     {
         return ClientPresenter::class;
+    }
+
+    public function boot()
+    {
+        $this->pushCriteria(app('Prettus\Repository\Criteria\RequestCriteria'));
     }
 }
